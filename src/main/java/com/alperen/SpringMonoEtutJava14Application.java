88eaedb5b3420a223915022bbf4039597be072a0
alperenikinci <alperen.ikinci@hotmail.com>
2024-05-16 18:31:35 +0300

    Initial commit

---
+++ src/main/java/com/alperen/SpringMonoEtutJava14Application.java
@@ -0,0 +1,12 @@
+package com.alperen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonoEtutJava14Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonoEtutJava14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/alperen/SpringMonoEtutJava14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonoEtutJava14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
